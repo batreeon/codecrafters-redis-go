89fe294cd49b59fb9211bb77c15bf53061a6cbf3
batreeon <1340551309@qq.com>
2024-11-16 03:29:29 +0800

    handle multiple clients

--- app/server.go
+++ app/server.go
@@ -30,23 +30,25 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	for {
-		buf := make([]byte, 128)
-		_, err := conn.Read(buf)
+	go func() {
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error read request: ", err.Error())
-			os.Exit(1)
-		} 
-		_, err = conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Println("Error write response: ", err.Error())
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-	}
+
+		for {
+			buf := make([]byte, 128)
+			_, err := conn.Read(buf)
+			if err != nil {
+				fmt.Println("Error read request: ", err.Error())
+				os.Exit(1)
+			}
+			_, err = conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				fmt.Println("Error write response: ", err.Error())
+				os.Exit(1)
+			}
+		}
+	}()
 }
