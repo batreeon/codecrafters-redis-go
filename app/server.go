1cdddbe8c9b66fc1cc17abaa8d47538d6df3059b
batreeon <1340551309@qq.com>
2024-11-16 03:33:17 +0800

    handle multiple clients

--- app/server.go
+++ app/server.go
@@ -30,13 +30,14 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 	}
-	go func() {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
 
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	
+	go func() {
 		for {
 			buf := make([]byte, 128)
 			_, err := conn.Read(buf)
