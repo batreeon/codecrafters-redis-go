aff3df89573ed01a7d672169b24a2f2f24664f10
batreeon <1340551309@qq.com>
2024-11-16 03:42:39 +0800

    handle multiple clients

--- app/server.go
+++ app/server.go
@@ -14,19 +14,8 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	defer l.Close()
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 	}
@@ -45,7 +34,7 @@ func main() {
 				_, err := conn.Read(buf)
 				if err != nil {
 					fmt.Println("Error read request: ", err.Error())
-					os.Exit(1)
+					return // return when one connection handler completed
 				}
 				_, err = conn.Write([]byte("+PONG\r\n"))
 				if err != nil {
