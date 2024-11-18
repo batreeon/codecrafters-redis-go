38c09913b4069deeb1042bc5a768dccf79172b94
batreeon <1340551309@qq.com>
2024-11-18 15:23:29 +0800

    stage 8 #ZG5: RDB file config: add log

--- app/config/config.go
+++ app/config/config.go
@@ -3,11 +3,13 @@ package config
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var Configs = map[string]string{}
 
 func SetConfigs() {
+	fmt.Println("cmd augs: ", os.Args)
 	dir := flag.String("dir", "", "the path to the directory where the RDB file is stored (example: /tmp/redis-data)")
 	dbfilename := flag.String("dbfilename", "", "the name of the RDB file (example: rdbfile)")
 	fmt.Printf("configs: dir=%s, dbfilename=%s", *dir, *dbfilename)
